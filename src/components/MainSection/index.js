7bf325e61f3c30695a485fe3f55b09988e0683a5
LucasGenova <glucasemanuel@gmail.com>
2022-06-04 23:44:42 -0300

    Add mainSection

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -4,7 +4,13 @@ import "./style.css";
 export function MainSection(){
   return(
     <main className="mainSection">
-      <h2>Seção principal</h2>
+            <img src="/semaped-banner.png" alt="Logo da Semana da Pedagogia"></img>
+
+            <div className="semaped-text">
+                <h2>Dias 07, 08 e 09 de Setembro</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, nulla ut luctus euismod, tortor erat sagittis nisi, vitae elementum massa nisi sit amet elit. Nam convallis non metus ac egestas. Nunc orci enim, tincidunt at diam non, luctus posuere nulla. tum massa nisi sit amet el i enim, tincidunt at di  euismod.</p>
+                <button>Inscreva-se</button>
+            </div>
     </main>
   )
 }
