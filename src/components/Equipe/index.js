960e8fc030f79142517efdc2f4c895545c6169c0
Otávio Augusto <tavinteixeira@hotmail.com>
2022-06-09 00:20:32 -0300

    Slider de equipe

--- src/components/Equipe/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Equipe(){
-  return(
-    <section>
-      <h2>Equipe</h2>
-    </section>
-  )
-}
---
+++ src/components/Equipe/swiper-item.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./equipe.css";
+
+export function SwiperItem() {
+    return (
+        <>
+            <div className="slider-item">
+                <img src="https://cdn.discordapp.com/attachments/593999593386278912/984289652959744081/unknown.png"  alt="equipe" />
+                <h1>Jorge bueno</h1>
+                <h2>Presidente</h2>
+            </div>
+        </>
+    )
+
+}
