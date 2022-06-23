aefff36496ad14805cf3fd09cf66f41db363d455
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2022-06-22 21:19:35 -0300

    Fix styles

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -10,7 +10,8 @@ export function MainSection(){
 
             <div className="semaped-text">
                 <h2>Dias 07, 08 e 09 de Setembro</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, nulla ut luctus euismod, tortor erat sagittis nisi, vitae elementum massa nisi sit amet elit. Nam convallis non metus ac egestas. Nunc orci enim, tincidunt at diam non, luctus posuere nulla. tum massa nisi sit amet el i enim, tincidunt at di  euismod.</p>
+                <p>Sejam bem-vindos a XIX semana de pedagogia UNESP-IBILCE, nesta edição retrataremos sobre o olhar crítico, reflexivo e representativo a etapa da educação infantil e anos iniciais do ensino fundamental ao longo de três conferências, oito minicursos, além da primeira mostra pedagógica, junto de diversas atividades culturais, sem falar nos deliciosos Coffee Breaks.
+                <br/>Vai perder essa? Eu aposto que não! Então clique aqui e faça parte da nossa família unespiana.</p>
                 <button>Inscreva-se</button>
             </div>
     </main>
