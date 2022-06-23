5e81add74105c4468306cf4c1bbc050c1d5ae7a4
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2022-06-22 23:42:34 -0300

    Fix styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,19 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [`gatsby-plugin-smoothscroll`],
+  plugins: [
+    `gatsby-plugin-smoothscroll`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    }
+  ],
 }
